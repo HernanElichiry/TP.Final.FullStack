0083c0c40a5afd7171be6d03db9332beb0f597d2
hansaguillon <hansaguillon@gmail.com>
2024-05-29 20:35:29 -0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 //import './App.css'
 import HomePage from './LandigPage/Home/HomePage'
 import "./../node_modules/bulma/css/bulma.css"
+import LoginForm from './LandigPage/Login/login'
 
 function App() {
   
   return (
     <>
-    <HomePage></HomePage>
-        
+     { //<HomePage></HomePage>
+      }  
+      <LoginForm></LoginForm>
     </>
   )
 }
