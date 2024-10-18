99358a87cd366db6bc702951b03800f04c017f73
hansaguillon <hansaguillon@gmail.com>
2024-10-18 11:50:59 -0300

    se ve la imagen

--- src/LandigPage/DetailPage/CourseDetail.jsx
+++ src/LandigPage/DetailPage/CourseDetail.jsx
@@ -1,48 +1,90 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { courses } from '../MockCourses/MockCourses';
-import './CourseDetail.css';
-import Testimonials from '../Components/Testimonials';
-import SubscriptionSection from '../Components/SuscriptionSection';
-import PaymentMethods from '../Components/PaymentMethods';
-import FAQ from '../Components/FAQ';
-import FooterApp from '../Footer/Footer';
-import Modalities from '../Components/Modalities';
-import instructorVideo from '../Components/instructot.video.mp4';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./CourseDetail.css";
+import Testimonials from "../Components/Testimonials";
+import SubscriptionSection from "../Components/SuscriptionSection";
+import PaymentMethods from "../Components/PaymentMethods";
+import FAQ from "../Components/FAQ";
+import FooterApp from "../Footer/Footer";
+import Modalities from "../Components/Modalities";
+import instructorVideo from "../Components/instructot.video.mp4";
 import foto from "../Components/foto.jpg";
 
 const CourseDetail = () => {
   const { id } = useParams(); // Obtener el ID del curso desde la URL
   const [course, setCourse] = useState(null); // Estado para almacenar el curso
+  const [imageUrl, setImageUrl] = useState(""); // Estado para almacenar la URL de la imagen
+  const [videoUrl, setVideoUrl] = useState(""); // Estado para almacenar la URL del video del instructor
 
   useEffect(() => {
     window.scrollTo(0, 0);
 
     const fetchCourse = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/courses/${id}`); // Solicitud GET para obtener el curso por ID
+        // Solicitud GET para obtener el curso por ID
+        const response = await fetch(`http://localhost:3000/courses/${id}`, {
+          method: "GET",
+          mode: "cors", // Permitir solicitudes entre diferentes orígenes
+          credentials: "include", // Incluir credenciales como cookies
+        });
+
+        if (!response.ok) {
+          throw new Error("Error al obtener el curso");
+        }
+
         const data = await response.json();
         setCourse(data); // Guardar el curso en el estado
+
+        // Construir la URL de la imagen del curso
+        const imageResponse = await fetch(
+          `http://localhost:3000/uploads/images/${data.media.filename}`,
+          {
+            method: "GET",
+            mode: "cors",
+            credentials: "include",
+          }
+        );
+
+        if (!imageResponse.ok) {
+          throw new Error("Error al obtener la imagen");
+        }
+
+        const videoResponse = await fetch(
+          `http://localhost:3000/uploads/videos/${data.media.videoname}`,
+          {
+            method: "GET",
+            mode: "cors",
+            credentials: "include",
+          }
+        );
+
+        if (!videoResponse.ok) {
+          throw new Error("Error al obtener la Video");
+        }
+        // Convertir la imagen a Blob y luego a URL
+        const imageBlob = await imageResponse.blob();
+        setImageUrl(URL.createObjectURL(imageBlob)); // Guardar la URL de la imagen
+        const videoBlob = await videoResponse.blob();
+        setVideoUrl(URL.createObjectURL(videoBlob)); // Guardar la URL del video
+
       } catch (error) {
-        console.error("Error al obtener el curso:", error);
+        console.error("Error al obtener el curso o la imagen:", error);
       }
     };
 
     fetchCourse();
   }, [id]); // Ejecutar cada vez que cambie el ID
 
-
   if (!course) {
     return <div>Curso no encontrado</div>;
   }
-  // Construir la URL de la imagen del curso
-  const imageUrl = `http://localhost:3000/uploads/images/${course.media.filename}`;
 
-  console.log(imageUrl);
   return (
     <div className="course-detail">
-      <div className="course-header" style={{ backgroundImage: `url(${imageUrl})` }}>
-      <img src={imageUrl} alt={course.title} />
+      <div
+        className="course-header"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      >
         <div className="course-overlay">
           <h1>{course.title}</h1>
           <h2>For {course.instructor.name}</h2>
@@ -51,7 +93,7 @@ const CourseDetail = () => {
       </div>
       <div className="course-content">
         <div className="course-main-content">
-          <section className='course-description'>
+          <section className="course-description">
             <h2>Sobre el curso</h2>
             <p>{course.description}</p>
             <h2>Requisitos</h2>
@@ -64,13 +106,27 @@ const CourseDetail = () => {
           <div className="course-info-section">
             <h2>Detalles del curso</h2>
             <div className="course-details">
-              <p><strong>Categoría:</strong> {course.category.name}</p>
-              <p><strong>Plataforma:</strong> {course.platform}</p>
-              <p><strong>Tipo de capacitación:</strong></p>
-              <p><strong>Valoración:</strong>  </p>
-              <p><strong>Topics:</strong> {course.courseTopics[0].topic.topic}</p>
-              <p><strong>Topics:</strong> {course.courseTopics[1].topic.topic}</p>
-              <p><strong>Precio:</strong> {course.price} dólares</p>
+              <p>
+                <strong>Categoría:</strong> {course.category.name}
+              </p>
+              <p>
+                <strong>Plataforma:</strong> {course.platform}
+              </p>
+              <p>
+                <strong>Tipo de capacitación:</strong>
+              </p>
+              <p>
+                <strong>Valoración:</strong>{" "}
+              </p>
+              <p>
+                <strong>Topics:</strong> {course.courseTopics[0].topic.topic}
+              </p>
+              <p>
+                <strong>Topics:</strong> {course.courseTopics[1].topic.topic}
+              </p>
+              <p>
+                <strong>Precio:</strong> {course.price} dólares
+              </p>
             </div>
           </div>
           <div className="course-price-section">
@@ -81,36 +137,41 @@ const CourseDetail = () => {
           </div>
         </div>
       </div>
-      <div className='course-plan'>
-        <h2><strong>Plan de estudio</strong></h2>
+      <div className="course-plan">
+        <h2>
+          <strong>Plan de estudio</strong>
+        </h2>
         <ul>
-          
-          {//PONER MAP
-          
+          {
+            //PONER MAP
           }
-          <li>Clase 1 : {course.classes[0].title}</li>
-          <li>Clase 2 {course.classes[1].title}</li>
-          <li>Clase 3 inserte aqui la informacion sobre la clase </li>
-          <li>Clase 4 inserte aqui la informacion sobre la clase </li>
-          <li>Clase 5 inserte aqui la informacion sobre la clase</li>
-          <li>Clase 6 inserte aqui la informacion sobre la clase</li>
-          <li>Clase 7 inserte aqui la informacion sobre la clase</li>
-          <li>Clase 8 inserte aqui la informacion sobre la clase</li>
-          <li>Clase 9 inserte aqui la informacion sobre la clase</li>
-          <li>Clase 10 inserte aqui la informacion sobre la clase</li>
+          {course.classes.map((clase, index) => (
+            <li key={index}>
+              Clase {index + 1}: {clase.title}
+            </li>
+          ))}
         </ul>
       </div>
       <div className="instructor-section">
         <div className="instructor-info">
-          <img src={foto} alt={course.instructor.name} className="instructor-photo" />
+          <img
+            src={foto}
+            alt={course.instructor.name}
+            className="instructor-photo"
+          />
           <div className="instructor-description">
             <h2>Sobre {course.instructor.name}</h2>
             <p>{course.description}</p>
           </div>
         </div>
         <div className="instructor-video">
-          <video controls>
-            <source src={instructorVideo} type="video/mp4" />
+          <video
+            controls
+            onError={(e) => {
+              console.error("Error al cargar el video", e);
+            }}
+          >
+            <source src={videoUrl} type="video/mp4" />
             Tu navegador no soporta el elemento de video.
           </video>
         </div>
@@ -126,4 +187,3 @@ const CourseDetail = () => {
 };
 
 export default CourseDetail;
-
