c7017e3318689cca15125402b22c511e3eebcde5
hansaguillon <hansaguillon@gmail.com>
2024-10-21 19:28:25 -0300

    fav en proffesor y usuario

--- src/LandigPage/User/Profesor/ProfessorFavorite.jsx
+++ src/LandigPage/User/Profesor/ProfessorFavorite.jsx
@@ -1,21 +1,21 @@
+
+
+
+
+import React, { useContext } from 'react';
+import { FavoritesContext } from '../FavoritesContext'; // Importar el contexto de favoritos
 import { FavCardProfessor } from "./ProfessorFavCard";
 
-const course = [
-  {
-    id: 1,
-    image:
-      "https://concepto.de/wp-content/uploads/2018/08/f%C3%ADsica-e1534938838719.jpg",
-    title: "Curso de Física",
-    description: "Descripción del curso de física",
-    rating: 5,
-    price: "****",
-  },
-];
 
 const FavoritesProfessor = () => {
+  const { favorites } = useContext(FavoritesContext);
+
+  
   return (
     <div>
-      <FavCardProfessor course={course} />
+      {favorites.map((course) => (
+        <FavCardProfessor key={course.id} course={course} />
+      ))}
     </div>
   );
 };
--- src/LandigPage/User/User/AlumnoFavCard.jsx
+++ src/LandigPage/User/User/AlumnoFavCard.jsx
@@ -1,22 +1,3 @@
-/*import React from "react";
-import "./AlumnoFavCard.css";
-import foto from "./fotos/progra.jpg";
-
-export const AlumnoFavCard = () => {
-  return (
-    <>
-      <div className="card-course-container">
-        <div className="card-course-image">
-          <img src={foto} />
-        </div>
-        <h2>Fisica Básica</h2>
-        <p>Rating: 5 </p>
-        <p>Precio: $125.000 </p>
-        <button className="button is-primary is-small">Buy It!</button>
-      </div>
-    </>
-  );
-};*/
 
 import React, { useContext, useEffect,useState } from 'react';
 import { useNavigate } from 'react-router-dom';
