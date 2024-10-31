7052a143831d1a2b49d4a263d2167efd4cef58aa
hansaguillon <hansaguillon@gmail.com>
2024-10-31 16:14:02 -0300

    actualziar datos de usuartio

--- src/LandigPage/Login/login.jsx
+++ src/LandigPage/Login/login.jsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../User/UserContext/UserContext";
 import "./login.css";
+import { message } from "antd";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const { login } = useUser();
+  
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -29,36 +31,41 @@ const LoginForm = () => {
         const data = await response.json();
 
         login(data.accest_token);
-
+        message.success("Inicio de sesión exitoso", 2); // Mensaje de éxito
         navigate("/")
         
       }catch (error) {
         console.error("Error en el login:", error);
-        alert("Error al iniciar sesión, verifique sus credenciales.");
+        message.error("Error al iniciar sesión, verifique sus credenciales."); // Mensaje de error
       }
     }
   
 
-  return (
-    <div>
+    return (
       <div className="app-container">
         <div className="login-container">
           <h2 className="login-h2">Iniciar sesión</h2>
           <form onSubmit={handleLogin} className="login-form">
             <div className="form-group">
-              <label htmlFor="email">Usuario : </label>
+              <label htmlFor="email">Usuario :</label>
               <input
                 id="email"
+                type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                placeholder="Ingrese su correo electrónico"
+                required
               />
             </div>
             <div className="form-group">
               <label htmlFor="password">Contraseña :</label>
               <input
                 id="password"
+                type="password" // Oculta el campo de la contraseña
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                placeholder="Ingrese su contraseña"
+                required
               />
             </div>
             <button type="submit" className="login-btn">
@@ -67,8 +74,7 @@ const LoginForm = () => {
           </form>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default LoginForm;
+    );
+  };
+  
+  export default LoginForm;
