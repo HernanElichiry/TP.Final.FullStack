8c748a35da3adb131e917be23c13437f02b35fa5
hansaguillon <hansaguillon@gmail.com>
2024-11-02 14:07:56 -0300

    profesor course

--- src/LandigPage/User/Profesor/MyCoursesProfessor.jsx
+++ src/LandigPage/User/Profesor/MyCoursesProfessor.jsx
@@ -3,16 +3,29 @@ import  ProfesorCoursesCard  from "./ProfesorCoursesCard/PorfesorCoursesCard";
 //import {course as mockCourses} from "./ProfesorCoursesCard/Mock";
 import './MyCoursesProfessor.css';
 import { useEffect, useState } from "react";
+import { useUser } from "../UserContext/UserContext";
+import Cookies from "js-cookie";
 
 
 const MyCoursesProfessor = () => {
 
   const [courses , setCourses] = useState([])
+  const user = useUser();
 
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await fetch("http://localhost:3000/courses"); // URL de la API
+        const token = Cookies.get("token"); // Obtener el token del usuario
+       const user_id = user.user.sub;
+   
+       const response = await fetch(`http://localhost:3000/courses/instructor/${user_id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include' // Incluir las cookies si es necesario
+      });
         const data = await response.json();
         setCourses(data);
   
@@ -28,14 +41,17 @@ const MyCoursesProfessor = () => {
 
   return (
     <div className="my-courses-container">
-      <h2></h2>
-      <div className="courses-grid">
-        {courses.map((course) => (
-          <ProfesorCoursesCard key={course.id} course={course} />
-        ))}
-        
-      </div>
-    </div>
+  <h2>Mis Cursos</h2>
+  <div className="courses-grid">
+    {courses.length > 0 ? (
+      courses.map((course) => (
+        <ProfesorCoursesCard key={course.id} course={course} />
+      ))
+    ) : (
+      <p className="no-courses-message">No tiene cursos cargados.</p>
+    )}
+  </div>
+</div>
   );
 };
 
