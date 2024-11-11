09b4d3ce9cc442b635024b27b9d58aa49b025c1d
hansaguillon <hansaguillon@gmail.com>
2024-11-11 12:48:20 -0300

    favoritos en buscar y buscar x cat, funcioan ya buscar por cat y buycourses en user

--- src/LandigPage/User/User/MyCoursesUser.jsx
+++ src/LandigPage/User/User/MyCoursesUser.jsx
@@ -1,17 +1,70 @@
+import { useEffect, useState } from "react";
+import { useUser } from "../UserContext/UserContext";
+import Cookies from "js-cookie";
 
-import foto from './fotos/Cursos.png';
-import cursoBarista from './fotos/curso barista.png';
+import {  AnimatePresence } from "framer-motion";
+import { PurchasedFavCard } from "../Profesor/PurchasedFavCard";
+import { PopupCourseDetail } from "../Profesor/PopupCourseDetail";
+
 
 
 const MyCoursesUser = () => {
-  return (
-    <div>
-      <h2>My Courses</h2>
-      <img src={foto} alt="imagen del curso"  />
-      <img src={cursoBarista} alt="imagen del curso"  />
-      {/* Aquí iría tu contenido de My Courses */}
-    </div>
-  );
-};
+  const [courses, setCourses] = useState([]);
+    const [selectedId, setSelectedId] = useState(null); // Estado para la tarjeta seleccionada
+    const user = useUser();
 
+    useEffect(() => {
+        const fetchCourses = async () => {
+            try {
+                const token = Cookies.get("token");
+                const user_id = user.user.sub;
+
+                const response = await fetch(`http://localhost:3000/buy-courses/${user_id}`, {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    },
+                    credentials: "include",
+                });
+                const data = await response.json();
+                setCourses(data);
+            } catch (error) {
+                console.error("Error al obtener los cursos:", error);
+            }
+        };
+
+        fetchCourses();
+    }, []);
+
+    return (
+        <div>
+            <h2>Mis Cursos</h2>
+            <div className="buycourses-grid">
+                {courses.length > 0 ? (
+                    courses.map((course) => (
+                        <PurchasedFavCard
+                            key={course.id}
+                            course={course}
+                            setSelectedId={setSelectedId}
+                        />
+                    ))
+                ) : (
+                    <p className="no-courses-message">No tiene cursos Comprados.</p>
+                )}
+            </div>
+
+            {/* Renderizar la ventana emergente al seleccionar un curso */}
+            <AnimatePresence>
+                {selectedId && (
+                      <PopupCourseDetail
+                        key={selectedId}
+                        course={courses.find((c) => c.id === selectedId)}
+                        onClose={() => setSelectedId(null)}
+                    />
+                )}
+            </AnimatePresence>
+        </div>
+    );
+};
 export default MyCoursesUser;
